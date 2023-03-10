f60dc44cba8154924ec06dbdecc506537aa9b041
MabyDiaz <mabeldiaz7680@gmail.com>
2023-03-10 00:34:04 -0300

    Declaración de variable modificada

--- src/egg_java1/ejercicio1.java
+++ src/egg_java1/ejercicio1.java
@@ -17,20 +17,16 @@ public class ejercicio1 {
      */
     public static void main(String[] args) {
        Scanner input = new Scanner(System.in);
-       
-       int num1;
-       int num2;
-       int suma;
-       
+              
        System.out.println("Ingrese el primer número");
         
-       num1 = input.nextInt();
+       int num1 = input.nextInt();
         
        System.out.println("Ingrese el segundo número");
         
-       num2 = input.nextInt();
+       int num2 = input.nextInt();
        
-       suma = num1 + num2;
+       int suma = num1 + num2;
        
        System.out.println("El resultado de sumar " + num1 + " y " + num2 + " es: " + suma);
         
